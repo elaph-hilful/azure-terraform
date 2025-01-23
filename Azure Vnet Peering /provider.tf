98fab8ead7e8c8218b675feaafc3831a6a93492b
Ashabul Elaph Hilful <hilful25@gmail.com>
2025-01-23 14:08:57 +0600

    Create provider.tf

---
+++ Azure Vnet Peering /provider.tf
@@ -0,0 +1,21 @@
+# Configure the Azure provider, you can have many
+# if you use azurerm provider, it's source is hashicorp/azurerm
+# short for registry.terraform.io/hashicorp/azurerm
+
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.8.0"
+    }
+  }
+
+  required_version = ">= 1.9.0"
+}
+# configures the provider
+
+provider "azurerm" {
+  features {}
+  subscription_id = "asv8cbe2daasg2-ds5cde-45vsd51-9esdb4d-ddbsdnse06c043c64"  
+}
